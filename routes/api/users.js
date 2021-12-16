cd1fe5d65d3087c28405c48671e677cd6f405b6c
Ajith Kumar Reddy <aajithkumarreddy@deloitte.com>
2021-12-16 13:18:17 +0530

    userjwttoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const {check, validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 //user model
 const User = require('../../models/User');
 
@@ -19,12 +21,14 @@ const errors = validationResult(req);
 if (!errors.isEmpty()){
     return res.status(400).json({errors: errors.array()});
 }
+
 const{name, email, password} = req.body;
+
 try{
     let user = await User.findOne({email});
     //See if user exists already
     if(user){
-        res.status(400).json({errors: [{msg:'User already exists'}]});
+        return res.status(400).json({errors: [{msg:'User already exists'}]});
     }
 //Get users garavatar
 const avatar = gravatar.url(email,{
@@ -46,13 +50,23 @@ user.password = await bcrypt.hash(password,salt);
 await user.save();
 
 //return jsonwebtoken
-res.send('User registered');
+const payload = {
+    user: {
+        id: user.id
+    }
+}
+jwt.sign(payload, 
+    config.get('jwtSecret'),
+    {expiresIn: 360000},
+   (err,token) => {
+       if(err) throw err;
+       res.json({token});
+   }
+    );
+
 }catch(err){
     console.error(err.message);
     res.status(500).send('Server Error');
-
 }
-
-
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(express.json({extended: false}));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server started on port ${PORT}'));
